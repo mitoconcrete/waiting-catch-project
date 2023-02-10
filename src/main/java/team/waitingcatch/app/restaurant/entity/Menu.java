e12a096f19fbf0af8ed7f2855b1ad1eed0bdbb9a
HanJungkyu <jk891113@naver.com>
2023-02-10 21:04:02 +0900

    [fix] Menu Entity에 name 옵션 제거

--- src/main/java/team/waitingcatch/app/restaurant/entity/Menu.java
+++ src/main/java/team/waitingcatch/app/restaurant/entity/Menu.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity(name = "menus")
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Menu extends TimeStamped {
