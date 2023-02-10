5d7f68967b7b272c49da01e9129a88cb490fee61
mitoconcrete <xogns98@gmail.com>
2023-02-10 20:50:49 +0900

    [feat] annotation 추가.

--- src/main/java/team/waitingcatch/app/user/service/UserServiceImpl.java
+++ src/main/java/team/waitingcatch/app/user/service/UserServiceImpl.java
@@ -3,8 +3,11 @@ package team.waitingcatch.app.user.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService, InternalUserService {
 }
 
