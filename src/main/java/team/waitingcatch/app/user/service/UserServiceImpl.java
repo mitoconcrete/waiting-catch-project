91444c5b4bcbba5d3017aac4185cfccea5f0d0e6
mitoconcrete <xogns98@gmail.com>
2023-02-10 20:47:00 +0900

    [style] 네이버 코드포맷을 적용합니다.

--- src/main/java/team/waitingcatch/app/user/service/UserServiceImpl.java
+++ src/main/java/team/waitingcatch/app/user/service/UserServiceImpl.java
@@ -5,5 +5,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService, InternalUserService{
+public class UserServiceImpl implements UserService, InternalUserService {
 }
+
