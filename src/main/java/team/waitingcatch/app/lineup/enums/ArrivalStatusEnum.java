35e6df5cb1ddd8b9de4273308a07fe6bad708ddc
sjcho <sshsjhome@gmail.com>
2023-02-10 20:17:38 +0900

    [feat] service에 @Transactional 추가
    service에 @Transactional 추가

--- src/main/java/team/waitingcatch/app/lineup/enums/ArrivalStatusEnum.java
+++ src/main/java/team/waitingcatch/app/lineup/enums/ArrivalStatusEnum.java
@@ -2,6 +2,6 @@ package team.waitingcatch.app.lineup.enums;
 
 public enum ArrivalStatusEnum {
 
-	WAIT, CALL, CANCEL, ARRIVE
+	WAIT, CALL, CANCEL, ARRIVAL
 
 }
--- src/main/java/team/waitingcatch/app/lineup/service/ReviewServiceImpl.java
+++ src/main/java/team/waitingcatch/app/lineup/service/ReviewServiceImpl.java
@@ -1,11 +1,13 @@
 package team.waitingcatch.app.lineup.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import team.waitingcatch.app.lineup.repository.ReviewRepository;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService, InternalReviewService {
 
