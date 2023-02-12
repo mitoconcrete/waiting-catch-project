60ccc2db47490464694c7509b2f6320348a2dcfc
rian <rianbowgift@gmail.com>
2023-02-12 14:12:50 +0900

    [refactor] 서비스파일 어노테이션 추가

--- src/main/java/team/waitingcatch/app/event/service/couponcreator/CouponCreatorServiceImpl.java
+++ src/main/java/team/waitingcatch/app/event/service/couponcreator/CouponCreatorServiceImpl.java
@@ -1,4 +1,13 @@
 package team.waitingcatch.app.event.service.couponcreator;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class CouponCreatorServiceImpl implements CouponCreatorService, InternalCouponCreatorService {
 }
--- src/main/java/team/waitingcatch/app/event/service/event/EventServiceImpl.java
+++ src/main/java/team/waitingcatch/app/event/service/event/EventServiceImpl.java
@@ -1,4 +1,13 @@
 package team.waitingcatch.app.event.service.event;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class EventServiceImpl implements EventService, InternalEventService {
 }
