74629483b112e5734a7e6bb65a0ce54c29443dfd
mitoconcrete <xogns98@gmail.com>
2023-02-13 02:06:48 +0900

    [feat] userDetailsService 에서 사용할 jpa 메소드를 추가합니다.

--- src/main/java/team/waitingcatch/app/user/repository/UserRepository.java
+++ src/main/java/team/waitingcatch/app/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package team.waitingcatch.app.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import team.waitingcatch.app.user.entitiy.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
 }
