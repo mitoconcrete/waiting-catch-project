b57201b625ed0f11b2897015006560c6018e3d46
mitoconcrete <xogns98@gmail.com>
2023-02-13 03:20:57 +0900

    [refactor] controller advice 의 위치를 수정합니다.

--- src/main/java/team/waitingcatch/app/exception/ApplicationExceptionController.java
+++ src/main/java/team/waitingcatch/app/exception/ApplicationExceptionController.java
@@ -1,4 +1,4 @@
-package team.waitingcatch.app.exception.controller;
+package team.waitingcatch.app.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
