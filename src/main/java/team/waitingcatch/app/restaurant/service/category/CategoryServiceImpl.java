0082e28cb31e03be290538d040b0648569c964d3
HanJungkyu <jk891113@naver.com>
2023-02-13 09:35:56 +0900

    [feat] ServiceImpl 클래스에 Transational 어노테이션 추가

--- src/main/java/team/waitingcatch/app/restaurant/service/category/CategoryServiceImpl.java
+++ src/main/java/team/waitingcatch/app/restaurant/service/category/CategoryServiceImpl.java
@@ -1,10 +1,12 @@
 package team.waitingcatch.app.restaurant.service.category;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CategoryServiceImpl implements CategoryService, InternalCategoryService {
 }
