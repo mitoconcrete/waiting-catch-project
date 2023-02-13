bcc8a25fed3ffeee41802548ca8306fc41c48790
HanJungkyu <jk891113@naver.com>
2023-02-13 14:16:41 +0900

    [test] #23 Category, CategoryServiceImpl 카테고리 생성 관련 테스트 코드를 추가합니다.

---
+++ src/test/java/team/waitingcatch/app/restaurant/entity/CategoryTest.java
@@ -0,0 +1,82 @@
+package team.waitingcatch.app.restaurant.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import team.waitingcatch.app.restaurant.dto.CreateCategoryRequest;
+
+class CategoryTest {
+
+	@Nested
+	@DisplayName("카테고리 생성")
+	class CreateCategory {
+
+		private Long id;
+		private Long parentId;
+		private String name;
+
+		@BeforeEach
+		void setup() {
+			parentId = null;
+			name = "한식";
+		}
+
+		@Test
+		@DisplayName("정상 케이스")
+		void createCategory_Normal() {
+			// given
+			CreateCategoryRequest request = new CreateCategoryRequest(parentId, name);
+
+			// when
+			Category category = Category.create(request);
+
+			// then
+			assertNull(category.getId());
+			assertEquals(parentId, category.getParentId());
+			assertEquals(name, category.getName());
+		}
+
+		@Nested
+		@DisplayName("실패 케이스")
+		class FailCases {
+
+			@Test
+			@DisplayName("카테고리명이 null인 경우")
+			void fail1() {
+				//given
+				name = null;
+				CreateCategoryRequest request = new CreateCategoryRequest(parentId, name);
+
+				// when
+				Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+					Category.create(request);
+				});
+
+				// then
+				assertEquals("카테고리명을 입력하세요.", exception.getMessage());
+			}
+
+			@Test
+			@DisplayName("카테고리명이 빈 문자열인 경우")
+			void fail2() {
+				// given
+				name = "";
+				CreateCategoryRequest request = new CreateCategoryRequest(parentId, name);
+
+				// when
+				Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+					Category.create(request);
+				});
+
+				// then
+				assertEquals("카테고리명에 빈값을 입력할 수 없습니다.", exception.getMessage());
+			}
+		}
+	}
+}
---
+++ src/test/java/team/waitingcatch/app/restaurant/service/category/CategoryServiceImplTest.java
@@ -0,0 +1,52 @@
+package team.waitingcatch.app.restaurant.service.category;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import team.waitingcatch.app.restaurant.dto.CreateCategoryRequest;
+import team.waitingcatch.app.restaurant.entity.Category;
+import team.waitingcatch.app.restaurant.repository.CategoryRepository;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceImplTest {
+
+	@Mock
+	private CategoryRepository categoryRepository;
+
+	@InjectMocks
+	private CategoryServiceImpl categoryService;
+
+	@Test
+	@DisplayName("부모 카테고리 생성")
+	void createParentCategory() {
+		// given
+		CreateCategoryRequest categoryRequest = new CreateCategoryRequest(null, "한식");
+
+		// when
+		categoryService.createCategory(categoryRequest);
+
+		// then
+		verify(categoryRepository, times(1)).save(any(Category.class));
+	}
+
+	@Test
+	@DisplayName("자식 카테고리 생성")
+	void createChildCategory() {
+		// given
+		CreateCategoryRequest categoryRequest = new CreateCategoryRequest(1L, "떡갈비");
+
+		// when
+		categoryService.createCategory(categoryRequest);
+
+		// then
+		verify(categoryRepository, times(1)).save(any(Category.class));
+	}
+}
