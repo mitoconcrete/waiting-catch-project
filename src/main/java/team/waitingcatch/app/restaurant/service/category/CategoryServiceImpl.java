4cd63940506d9aa4b7d9d7f552b7ab04730d74b5
HanJungkyu <jk891113@naver.com>
2023-02-14 12:07:04 +0900

    [feat] #121 update 할때 save 쿼리메소드를 삭제합니다. (dirty checking)

--- src/main/java/team/waitingcatch/app/restaurant/service/category/CategoryServiceImpl.java
+++ src/main/java/team/waitingcatch/app/restaurant/service/category/CategoryServiceImpl.java
@@ -48,7 +48,6 @@ public class CategoryServiceImpl implements CategoryService, InternalCategorySer
 	public void updateCategory(UpdateCategoryServiceRequest serviceRequest) {
 		Category category = this._getCategory(serviceRequest.getCategoryId());
 		category.update(serviceRequest);
-		categoryRepository.save(category);
 	}
 
 	@Override
