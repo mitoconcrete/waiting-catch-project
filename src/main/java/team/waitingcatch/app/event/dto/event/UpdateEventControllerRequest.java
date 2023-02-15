7cd7f63fc2377a3ea1ebebf967faa08162e93640
rian <rianbowgift@gmail.com>
2023-02-15 09:55:03 +0900

    [refactor] validation 처리

--- src/main/java/team/waitingcatch/app/event/dto/event/UpdateEventControllerRequest.java
+++ src/main/java/team/waitingcatch/app/event/dto/event/UpdateEventControllerRequest.java
@@ -1,8 +1,10 @@
 package team.waitingcatch.app.event.dto.event;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UpdateEventControllerRequest {
 	private String name;
 	private String eventStartDate;
