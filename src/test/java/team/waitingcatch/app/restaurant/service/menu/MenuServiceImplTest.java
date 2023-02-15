271ec04a19b8cc14898b4684786f623387f85f5f
HanJungkyu <jk891113@naver.com>
2023-02-16 00:34:57 +0900

    [test] 자신의 레스토랑 메뉴 조회 기능의 테스트 코드를 추가합니다.

--- src/test/java/team/waitingcatch/app/restaurant/service/menu/MenuServiceImplTest.java
+++ src/test/java/team/waitingcatch/app/restaurant/service/menu/MenuServiceImplTest.java
@@ -1,5 +1,6 @@
 package team.waitingcatch.app.restaurant.service.menu;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -7,6 +8,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -19,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import team.waitingcatch.app.common.util.S3Uploader;
 import team.waitingcatch.app.restaurant.dto.menu.CreateMenuServiceRequest;
+import team.waitingcatch.app.restaurant.dto.menu.MenuResponse;
 import team.waitingcatch.app.restaurant.entity.Menu;
 import team.waitingcatch.app.restaurant.entity.Restaurant;
 import team.waitingcatch.app.restaurant.repository.MenuRepository;
@@ -80,5 +84,23 @@ class MenuServiceImplTest {
 			// then
 			verify(menuRepository, times(1)).save(any(Menu.class));
 		}
+	}
+
+	@Test
+	@DisplayName("자신의 레스토랑 메뉴 조회")
+	void getMyRestaurantMenus() {
+		// given
+		List<Menu> menus = new ArrayList<>();
+		Menu menu = mock(Menu.class);
+		menus.add(menu);
+
+		when(menu.getName()).thenReturn("aaa");
+		when(menuRepository.findAllByRestaurantId(any(Long.class))).thenReturn(menus);
+
+		// when
+		List<MenuResponse> responses = menuService.getMyRestaurantMenus(any(Long.class));
+
+		// then
+		assertEquals("aaa", responses.get(0).getName());
 	}
 }
