d1020adc2232e53419fd2025f65d7aae6eb85a56
mitoconcrete <xogns98@gmail.com>
2023-02-15 15:14:57 +0900

    [feat] 타도메인에서의 사용을 위해 getter 를 추가합니다.

--- src/main/java/team/waitingcatch/app/user/entitiy/UserDetailsImpl.java
+++ src/main/java/team/waitingcatch/app/user/entitiy/UserDetailsImpl.java
@@ -26,6 +26,10 @@ public class UserDetailsImpl implements UserDetails {
 		return authorities;
 	}
 
+	public Long getId() {
+		return user.getId();
+	}
+
 	@Override
 	public String getPassword() {
 		return user.getPassword();
