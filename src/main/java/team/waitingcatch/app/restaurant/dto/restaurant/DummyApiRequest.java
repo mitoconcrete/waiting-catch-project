3aec9cd1d21942b72dedf7d300bf9c73157354cb
developer-Park <83831110+developer-Park@users.noreply.github.com>
2023-02-17 02:32:23 +0900

    [feat] #98 레스토랑 Test Data 가져오기 API
    레스토랑 Test Data 가져오기 API

---
+++ src/main/java/team/waitingcatch/app/restaurant/dto/restaurant/DummyApiRequest.java
@@ -0,0 +1,12 @@
+package team.waitingcatch.app.restaurant.dto.restaurant;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DummyApiRequest {
+	private String x;
+	private String y;
+	private int page;
+}
