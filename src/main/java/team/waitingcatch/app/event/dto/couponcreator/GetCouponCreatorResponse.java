01abef719ecc00aaf2fa6a13651e26a5fe2be657
rian <rianbowgift@gmail.com>
2023-02-18 16:43:06 +0900

    [refactor] 쿠폰생성자 반환시 Id값도 반환하게 추가

--- src/main/java/team/waitingcatch/app/event/dto/couponcreator/GetCouponCreatorResponse.java
+++ src/main/java/team/waitingcatch/app/event/dto/couponcreator/GetCouponCreatorResponse.java
@@ -6,11 +6,13 @@ import team.waitingcatch.app.event.enums.CouponTypeEnum;
 
 @Getter
 public class GetCouponCreatorResponse {
+	private Long id;
 	private String name;
 	private int discountPrice;
 	private CouponTypeEnum discountType;
 
 	public GetCouponCreatorResponse(CouponCreator couponCreator) {
+		this.id = couponCreator.getId();
 		this.name = couponCreator.getName();
 		this.discountPrice = couponCreator.getDiscountPrice();
 		this.discountType = couponCreator.getDiscountType();
