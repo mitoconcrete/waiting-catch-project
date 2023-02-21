b0c50855b799b42a139904eb6b73947f1e383087
HanJungkyu <jk891113@naver.com>
2023-02-21 11:39:20 +0900

    [merge] 병합

--- src/main/java/team/waitingcatch/app/exception/ApplicationExceptionController.java
+++ src/main/java/team/waitingcatch/app/exception/ApplicationExceptionController.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,14 +23,19 @@ public class ApplicationExceptionController {
 
 	@ExceptionHandler({IllegalArgumentException.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public BasicExceptionResponse handleIllegalArgumentException(IllegalArgumentException e) {
-		return new BasicExceptionResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+	public BasicExceptionResponse illegalArgumentExceptionHandler(IllegalArgumentException ex) {
+		return new BasicExceptionResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
 	}
 
 	@ExceptionHandler({DataIntegrityViolationException.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public BasicExceptionResponse handleDataIntegrityViolationException() {
-		return new BasicExceptionResponse(HttpStatus.BAD_REQUEST,
-			"데이터 무결성 오류 발생 : Unique 한 데이터를 넣어주어야합니다.");
+	public BasicExceptionResponse integrityExceptionHandler(DataIntegrityViolationException ex) {
+		return new BasicExceptionResponse(HttpStatus.BAD_REQUEST, "데이터 무결성 오류 발생 : Unique 한 데이터를 넣어주어야합니다.");
+	}
+
+	@ExceptionHandler({HttpMessageNotReadableException.class})
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public BasicExceptionResponse httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException ex) {
+		return new BasicExceptionResponse(HttpStatus.BAD_REQUEST, ex.getRootCause().getMessage());
 	}
 }
