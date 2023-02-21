16000dc6e6e12607717ea41549d81d5fae0d7812
sjcho <sshsjhome@gmail.com>
2023-02-22 02:54:47 +0900

    [fix] JPA 사용자 정의 인터페이스 상속
    JPA 사용자 정의 인터페이스 상속

--- src/main/java/team/waitingcatch/app/restaurant/repository/RestaurantInfoRepository.java
+++ src/main/java/team/waitingcatch/app/restaurant/repository/RestaurantInfoRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import team.waitingcatch.app.restaurant.entity.RestaurantInfo;
 import team.waitingcatch.app.restaurant.repository.querydsl.RestaurantInfoRepositoryCustom;
 
-public interface RestaurantInfoRepository extends JpaRepository<RestaurantInfo, Long> {
+public interface RestaurantInfoRepository extends JpaRepository<RestaurantInfo, Long>, RestaurantInfoRepositoryCustom {
 	@Query("select ri from RestaurantInfo ri where ri.restaurant.id = :restaurantId")
 	Optional<RestaurantInfo> findByRestaurantId(@Param("restaurantId") Long restaurantId);
 }
