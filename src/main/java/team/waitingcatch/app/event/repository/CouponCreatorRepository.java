f47f3e3a042cf1520c60e4ee4d32ffc34e5fb178
HanJungkyu <jk891113@naver.com>
2023-02-22 00:31:46 +0900

    [feat] #4 광역 이벤트 목록 조회 기능 추가, 광역 이벤트 쿠폰 생성자 조회 기능 추가

--- src/main/java/team/waitingcatch/app/event/repository/CouponCreatorRepository.java
+++ src/main/java/team/waitingcatch/app/event/repository/CouponCreatorRepository.java
@@ -1,8 +1,14 @@
 package team.waitingcatch.app.event.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import io.lettuce.core.dynamic.annotation.Param;
 import team.waitingcatch.app.event.entity.CouponCreator;
 
 public interface CouponCreatorRepository extends JpaRepository<CouponCreator, Long> {
+	@Query(value = "select c from CouponCreator c where c.event.id = :eventId and c.isDeleted = false")
+	List<CouponCreator> findAllByEventId(@Param("eventId") Long eventId);
 }
