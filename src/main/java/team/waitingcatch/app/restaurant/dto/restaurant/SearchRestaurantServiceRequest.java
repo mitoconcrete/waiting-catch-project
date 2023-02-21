4a9e3a1ec1f140d1c7138789608c401166fb950d
HanJungkyu <jk891113@naver.com>
2023-02-21 11:43:55 +0900

    [refactor] #133 SearchRestaurantServiceRequest에 NotBlank 어노테이션 제거

--- src/main/java/team/waitingcatch/app/restaurant/dto/restaurant/SearchRestaurantServiceRequest.java
+++ src/main/java/team/waitingcatch/app/restaurant/dto/restaurant/SearchRestaurantServiceRequest.java
@@ -1,14 +1,11 @@
 package team.waitingcatch.app.restaurant.dto.restaurant;
 
-import javax.validation.constraints.NotBlank;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class SearchRestaurantServiceRequest {
-	@NotBlank(message = "키워드를 입력해 주세요.")
 	private final String keyword;
 	private final double latitude;
 	private final double longitude;
