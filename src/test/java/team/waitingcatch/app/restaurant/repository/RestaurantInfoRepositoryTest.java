b0d8ae6ba0a3aa966cb0cfe8cc9963238b2f3d84
mitoconcrete <xogns98@gmail.com>
2023-02-21 19:13:08 +0900

    [hotfix] 오류발생파일 수정 및 강제적용

---
+++ src/test/java/team/waitingcatch/app/restaurant/repository/RestaurantInfoRepositoryTest.java
@@ -0,0 +1,35 @@
+package team.waitingcatch.app.restaurant.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import team.waitingcatch.app.restaurant.dto.restaurant.RestaurantsWithinRadiusJpaResponse;
+import team.waitingcatch.app.restaurant.dto.restaurant.SearchRestaurantJpaResponse;
+
+@SpringBootTest
+class RestaurantInfoRepositoryTest {
+	@Autowired
+	private RestaurantInfoRepository restaurantInfoRepository;
+
+	@Test
+	void findRestaurantsBySearchKeywordsContains() {
+		List<SearchRestaurantJpaResponse> jpaResponses =
+			restaurantInfoRepository.findRestaurantsBySearchKeywordsContaining("keywords");
+
+		assertEquals(5, jpaResponses.size());
+	}
+
+	@Test
+	void findRestaurantsByDistance() {
+		List<RestaurantsWithinRadiusJpaResponse> restaurants =
+			restaurantInfoRepository.findRestaurantsByDistance(37.339141, 127.082427, 3);
+
+		assertEquals(3, restaurants.size());
+	}
+
+}
