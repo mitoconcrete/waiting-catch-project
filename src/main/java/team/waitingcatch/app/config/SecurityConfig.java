f3febd6af4db10680ef98827400a7afa87244bdb
mitoconcrete <xogns98@gmail.com>
2023-02-23 00:42:54 +0900

    [fix] 불필요한 설정 파일 제거

--- src/main/java/team/waitingcatch/app/config/SecurityConfig.java
+++ src/main/java/team/waitingcatch/app/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -14,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -51,9 +49,6 @@ public class SecurityConfig implements WebMvcConfigurer {
 		// CSRF 설정
 		http.csrf().disable();
 
-		http.authorizeRequests()
-			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
-
 		//프론트엔드 접속 시, CORS 를 허용합니다.
 		http.cors().configurationSource(request -> {
 
@@ -84,13 +79,5 @@ public class SecurityConfig implements WebMvcConfigurer {
 		http.exceptionHandling().accessDeniedHandler(accessDeniedHandler);
 
 		return http.build();
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "http://localhost:5500")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
-			.exposedHeaders("Authorization");
 	}
 }
