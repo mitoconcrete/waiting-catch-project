34acccb0c0c70a8bd6cdafb839a237671ccdc48c
sjcho <sshsjhome@gmail.com>
2023-02-22 23:32:07 +0900

    [feat] 판매자 승인 상태 enum에 value 추가
    판매자 승인 상태 enum에 value 추가

--- src/main/java/team/waitingcatch/app/restaurant/enums/AcceptedStatusEnum.java
+++ src/main/java/team/waitingcatch/app/restaurant/enums/AcceptedStatusEnum.java
@@ -1,5 +1,17 @@
 package team.waitingcatch.app.restaurant.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum AcceptedStatusEnum {
-	WAIT, REJECTION, APPROVAL, CANCEL
+	WAIT("대기"),
+	REJECT("거절"),
+	APPROVE("승인"),
+	CANCEL("취소");
+
+	private final String value;
+
+	AcceptedStatusEnum(String value) {
+		this.value = value;
+	}
 }
