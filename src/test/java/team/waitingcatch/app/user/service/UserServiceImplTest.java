4674f8eeba32573e7df73aab5c8dba66eb33b029
mitoconcrete <xogns98@gmail.com>
2023-03-01 01:33:21 +0900

    [test] customer list 가져오기 + paging 잘 되는지 확인 + user만 잘가져오는지 확인

--- src/test/java/team/waitingcatch/app/user/service/UserServiceImplTest.java
+++ src/test/java/team/waitingcatch/app/user/service/UserServiceImplTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -90,4 +91,51 @@ class UserServiceImplTest {
 		assertTrue(killedAccessTokenRepository.existsById(loginResponse.getAccessToken().substring(7)));
 	}
 
+	@Test
+	@DisplayName("유저 목록 등록 후 페이징 처리")
+	void getCustomers() {
+		// given
+		String[] usernameList = {
+			"xogns1", "xogns2", "xogns3", "xogns4"
+		};
+
+		String[] nicknameList = {
+			"xogns1", "xogns2", "xogns3", "xogns4"
+		};
+
+		String[] pnList = {
+			"010-1001-1234", "010-1002-1234", "010-1003-1234", "010-1004-1234"
+		};
+
+		String[] emailList = {
+			"1@email.com", "2@email.com", "3@email.com", "4@email.com"
+		};
+
+		for (int i = 0; i < usernameList.length; i++) {
+			var user = new User(UserRoleEnum.USER, "김태훈", emailList[i], usernameList[i], "Test1234!",
+				nicknameList[i], pnList[i]);
+			userRepository.save(user);
+		}
+
+		// create seller , admin
+		User seller = new User(UserRoleEnum.SELLER, "김태훈", "1@seller.com", "seller01", "Test1234!", "seller01",
+			"010-1234-0001");
+		User admin = new User(UserRoleEnum.ADMIN, "김태훈", "1@admin.com", "admin01", "Test1234!", "seller01",
+			"010-1234-0002");
+		// when&then
+		Pageable pageable1 = Pageable.ofSize(3).withPage(0);
+		Pageable pageable2 = Pageable.ofSize(3).withPage(1);
+		Pageable pageable3 = Pageable.ofSize(3).withPage(2);
+
+		Pageable pageable4 = Pageable.ofSize(5).withPage(0);
+		Pageable pageable5 = Pageable.ofSize(5).withPage(1);
+		Pageable pageable6 = Pageable.ofSize(5).withPage(2);
+		assertEquals(userService.getCustomers(pageable1).size(), 3);
+		assertEquals(userService.getCustomers(pageable2).size(), 2);
+		assertEquals(userService.getCustomers(pageable3).size(), 0);
+
+		assertEquals(userService.getCustomers(pageable4).size(), 5);
+		assertEquals(userService.getCustomers(pageable5).size(), 0);
+		assertEquals(userService.getCustomers(pageable6).size(), 0);
+	}
 }
