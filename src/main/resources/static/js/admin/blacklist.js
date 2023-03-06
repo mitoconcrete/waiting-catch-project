0625a7c9e0b36d539816695e00eb2c32fca485f0
HanJungkyu <jk891113@naver.com>
2023-03-07 03:52:03 +0900

    [feat] #198 admin 블랙리스트 페이징 기능 구현(프론트)

--- src/main/resources/static/js/admin/blacklist.js
+++ src/main/resources/static/js/admin/blacklist.js
@@ -86,22 +86,30 @@ $(document).ready(function () {
     $.ajax({
         type: 'GET',
         url: `/api/admin/restaurants/blacklist-demands`,
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", auth);
-        },
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader("Authorization", auth);
+        // },
         success: function (data, status, response) {
             if (response.getResponseHeader("Authorization")) {
                 document.cookie =
                     'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
                     "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
             }
-            let responseData = data.data;
+            let responseData = data.data.content;
+            let totalPage = data.data.totalPages;
             console.log(responseData)
+            console.log(data.data);
+            console.log(totalPage)
             if (status === 'success') {
                 for (let i = 0; i < responseData.length; i++) {
                     let responseDto = responseData[i];
                     let tempHtml = getBlacklistRequest(responseDto);
                     $('#blacklistDemand').append(tempHtml);
+                }
+                for (let i = 0; i < totalPage; i++) {
+                    let page = i + 1;
+                    let tempHtml = getBlacklistRequestPagination(page);
+                    $('#blacklistRequestPagination').append(tempHtml);
                 }
             }
         },
@@ -119,22 +127,30 @@ $(document).ready(function () {
     $.ajax({
         type: 'GET',
         url: `/api/admin/blacklists`,
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", auth);
-        },
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader("Authorization", auth);
+        // },
         success: function (data, status, response) {
             if (response.getResponseHeader("Authorization")) {
                 document.cookie =
                     'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
                     "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
             }
-            let responseData = data.data;
+            let responseData = data.data.content;
+            let totalPage = data.data.totalPages;
             console.log(responseData)
+            console.log(data.data);
+            console.log(totalPage);
             if (status === 'success') {
                 for (let i = 0; i < responseData.length; i++) {
                     let responseDto = responseData[i];
                     let tempHtml = getBlacklist(responseDto);
                     $('#blacklist').append(tempHtml);
+                }
+                for (let i = 0; i < totalPage; i++) {
+                    let page = i + 1;
+                    let tempHtml = getBlacklistPagination(page);
+                    $('#blacklistPagination').append(tempHtml);
                 }
             }
         },
@@ -149,3 +165,83 @@ $(document).ready(function () {
         }
     })
 });
+
+function getBlacklistRequestPage(page) {
+    $('#blacklistDemand').empty();
+    $('#blacklistRequestPagination').empty();
+
+    let pageNumber = page - 1;
+
+    $.ajax({
+        type: 'GET',
+        url: `/api/admin/restaurants/blacklist-demands?page=` + pageNumber,
+        success: function (data, status, response) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            let responseData = data.data.content;
+            let totalPage = data.data.totalPages;
+            console.log(responseData);
+            console.log(data.data);
+            console.log(totalPage);
+            if (status === 'success') {
+                for (let i = 0; i < responseData.length; i++) {
+                    let responseDto = responseData[i];
+                    let tempHtml = getBlacklistRequest(responseDto);
+                    $('#blacklistDemand').append(tempHtml);
+                }
+                for (let i = 0; i < totalPage; i++) {
+                    let page = i + 1;
+                    let tempHtml = getBlacklistRequestPagination(page);
+                    $('#blacklistRequestPagination').append(tempHtml);
+                }
+            }
+        }
+    })
+}
+
+function getBlacklistPage(page) {
+    $('#blacklist').empty();
+    $('#blacklistPagination').empty();
+
+    let pageNumber = page - 1;
+
+    $.ajax({
+        type: 'GET',
+        url: `api/admin/blacklists?page=` + pageNumber,
+        success: function (data, status, response) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            let responseData = data.data.content;
+            let totalpage = data.data.totalPages;
+            console.log(responseData);
+            console.log(data.data);
+            console.log(totalpage);
+            if (status === 'success') {
+                for (let i = 0; i < responseData.length; i++) {
+                    let responseDto = responseData[i];
+                    let tempHtml = getBlacklist(responseDto);
+                    $('#blacklist').append(tempHtml);
+                }
+                for (let i = 0; i < totalpage; i++) {
+                    let page = i + 1;
+                    let tempHtml = getBlacklistPagination(page);
+                    $('#blacklistPagination').append(tempHtml);
+                }
+            }
+        }
+    })
+}
+
+function getBlacklistRequestPagination(page) {
+    return `<li class="page-item"><a onclick="getBlacklistRequestPage(${page})" style="color: #8B60C7" class="page-link" href="#">${page}</a></li>`
+}
+
+function getBlacklistPagination(page) {
+    return `<li class="page-item"><a onclick="getBlacklistPage(${page})" style="color: #8B60C7" class="page-link" href="#">${page}</a></li>`
+}
