98944c11770f11ea00f13bc8ed04149923f8112d
mitoconcrete <xogns98@gmail.com>
2023-03-06 22:18:45 +0900

    [refacor] 로그아웃 전에 세션이 유지되도록 설정.

--- src/main/resources/static/js/admin/blacklist.js
+++ src/main/resources/static/js/admin/blacklist.js
@@ -31,97 +31,121 @@ function getBlacklist(responseDto) {
 }
 
 function approveBlacklistRequest(blacklistRequestId) {
-    const auth = getToken();
-    console.log(blacklistRequestId);
-
-    if (auth !== '') {
-        $.ajax({
-            type: 'POST',
-            url: `/api/admin/restaurants/blacklist-demands/` + blacklistRequestId,
-            data: {},
-            success: function (response, status) {
-                alert("승인 완료")
-                window.location.reload();
-            },
-            error(error, response) {
-                console.error(error);
-                // window.location.href = host + "/admin/templates/login"
-                window.location.reload();
+    $.ajax({
+        type: 'POST',
+        url: `/api/admin/restaurants/blacklist-demands/` + blacklistRequestId,
+        data: {},
+        success: function (data, status, response) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
             }
-        });
-    }
+            alert("승인 완료")
+            window.location.reload();
+        },
+        error: function (response, status, error) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            window.location.reload();
+        }
+    });
 }
 
 function rejectBlacklistRequest(blacklistRequestId) {
-    const auth = getToken();
-
-    if (auth !== '') {
-        $.ajax({
-            type: 'PUT',
-            url: `/api/admin/restaurants/blacklist-demand/` + blacklistRequestId,
-            data: {},
-            success: function (response, status) {
-                if (status === 'success') {
-                    alert("요청 거절 완료")
-                    window.location.reload();
-                }
-            },
-            error(error, response) {
-                console.error(error);
+    $.ajax({
+        type: 'PUT',
+        url: `/api/admin/restaurants/blacklist-demand/` + blacklistRequestId,
+        data: {},
+        success: function (data, status, response) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            if (status === 'success') {
+                alert("요청 거절 완료")
                 window.location.reload();
             }
-        })
-    }
+        },
+        error: function (response, status, error) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            window.location.reload();
+        }
+    })
 }
 
 $(document).ready(function () {
-    const auth = getToken();
-
-    if (auth !== '') {
-        $.ajax({
-            type: 'GET',
-            url: `/api/admin/restaurants/blacklist-demands`,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Authorization", auth);
-            },
-            success: function (response, status) {
-                let responseData = response.data;
-                console.log(responseData)
-                if (status === 'success') {
-                    for (let i = 0; i < responseData.length; i++) {
-                        let responseDto = responseData[i];
-                        let tempHtml = getBlacklistRequest(responseDto);
-                        $('#blacklistDemand').append(tempHtml);
-                    }
-                }
-            },
-            error(error, response) {
-                console.error(error);
-                // window.location.href = host + "/admin/templates/login";
+    $.ajax({
+        type: 'GET',
+        url: `/api/admin/restaurants/blacklist-demands`,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", auth);
+        },
+        success: function (data, status, response) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
             }
-        });
-
-        $.ajax({
-            type: 'GET',
-            url: `/api/admin/blacklists`,
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Authorization", auth);
-            },
-            success: function (response, status) {
-                let responseData = response.data;
-                console.log(responseData)
-                if (status === 'success') {
-                    for (let i = 0; i < responseData.length; i++) {
-                        let responseDto = responseData[i];
-                        let tempHtml = getBlacklist(responseDto);
-                        $('#blacklist').append(tempHtml);
-                    }
+            let responseData = data.data;
+            console.log(responseData)
+            if (status === 'success') {
+                for (let i = 0; i < responseData.length; i++) {
+                    let responseDto = responseData[i];
+                    let tempHtml = getBlacklistRequest(responseDto);
+                    $('#blacklistDemand').append(tempHtml);
                 }
-            },
-            error(error, response) {
-                console.error(error);
-                // window.location.href = host + "/admin/templates/login";
             }
-        })
-    }
+        },
+        error: function (response, status, error) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            console.error(error);
+            // window.location.href = host + "/admin/templates/login";
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: `/api/admin/blacklists`,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", auth);
+        },
+        success: function (data, status, response) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            let responseData = data.data;
+            console.log(responseData)
+            if (status === 'success') {
+                for (let i = 0; i < responseData.length; i++) {
+                    let responseDto = responseData[i];
+                    let tempHtml = getBlacklist(responseDto);
+                    $('#blacklist').append(tempHtml);
+                }
+            }
+        },
+        error: function (response, status, error) {
+            if (response.getResponseHeader("Authorization")) {
+                document.cookie =
+                    'Authorization' + "=" + response.getResponseHeader("Authorization") + "; " +
+                    "path=/; expires=" + (new Date().getTime() + 30 * 60000) + ";";
+            }
+            console.error(error);
+            // window.location.href = host + "/admin/templates/login";
+        }
+    })
 });
