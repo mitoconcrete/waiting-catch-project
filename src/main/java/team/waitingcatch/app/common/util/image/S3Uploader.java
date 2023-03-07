6ec910d55c1471ea85d836459e07603c352082b8
sjcho <sshsjhome@gmail.com>
2023-03-07 23:15:58 +0900

    [feat] 배포를 위한 이미지 저장소 S3로 변경
    로컬 -> S3

--- src/main/java/team/waitingcatch/app/common/util/image/S3Uploader.java
+++ src/main/java/team/waitingcatch/app/common/util/image/S3Uploader.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -15,7 +16,7 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import lombok.RequiredArgsConstructor;
 
-// @Service
+@Service
 @RequiredArgsConstructor
 public class S3Uploader implements ImageUploader {
 	private final AmazonS3Client amazonS3Client;
