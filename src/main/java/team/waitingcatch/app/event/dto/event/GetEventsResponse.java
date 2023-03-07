b85b3d1e16fe1b2e54e3e5e8ebdec43261980208
rian <rianbowgift@gmail.com>
2023-03-07 20:12:32 +0900

    [refactor] 쿠폰+쿠폰생성자 수정

--- src/main/java/team/waitingcatch/app/event/dto/event/GetEventsResponse.java
+++ src/main/java/team/waitingcatch/app/event/dto/event/GetEventsResponse.java
@@ -2,9 +2,11 @@ package team.waitingcatch.app.event.dto.event;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.Getter;
 import team.waitingcatch.app.event.dto.couponcreator.GetCouponCreatorResponse;
+import team.waitingcatch.app.event.entity.CouponCreator;
 import team.waitingcatch.app.event.entity.Event;
 
 @Getter
@@ -15,11 +17,22 @@ public class GetEventsResponse {
 	private LocalDateTime eventEndDate;
 	private List<GetCouponCreatorResponse> couponCreators;
 
-	public GetEventsResponse(Event event, List<GetCouponCreatorResponse> getCouponCreatorResponse) {
+	public GetEventsResponse(Event event, List<CouponCreator> couponCreators) {
 		this.eventId = event.getId();
 		this.name = event.getName();
 		this.eventStartDate = event.getEventStartDate();
 		this.eventEndDate = event.getEventEndDate();
-		this.couponCreators = getCouponCreatorResponse;
+		this.couponCreators = couponCreators.stream().map(GetCouponCreatorResponse::new).collect(Collectors.toList());
+	}
+
+	public GetEventsResponse(Long eventId, String name, LocalDateTime eventStartDate, LocalDateTime eventEndDate) {
+		this.eventId = eventId;
+		this.name = name;
+		this.eventStartDate = eventStartDate;
+		this.eventEndDate = eventEndDate;
+	}
+
+	public void GetEventsSettingResponse(List<GetCouponCreatorResponse> couponCreators) {
+		this.couponCreators = couponCreators;
 	}
 }
