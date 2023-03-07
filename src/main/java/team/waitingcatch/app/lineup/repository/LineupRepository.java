459aba948862fc548b72e0b07d710ae39de2f63c
sjcho <sshsjhome@gmail.com>
2023-03-07 23:01:28 +0900

    [refactor] lineup 데이터 lineup_history 이관 시 페이징 처리
    매일 5시 스케줄링 된 lineup 데이터를 lineup_history으로 이관 시 페이징 처리

--- src/main/java/team/waitingcatch/app/lineup/repository/LineupRepository.java
+++ src/main/java/team/waitingcatch/app/lineup/repository/LineupRepository.java
@@ -9,21 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import team.waitingcatch.app.lineup.entity.Lineup;
-import team.waitingcatch.app.lineup.enums.ArrivalStatusEnum;
 
 public interface LineupRepository extends JpaRepository<Lineup, Long>, LineupRepositoryCustom {
-	@Query("select l from Lineup l where l.user.id = :userId")
-	List<Lineup> findAllByUserId(@Param("userId") Long userId);
-
 	@Query("select l from Lineup l join fetch l.user where l.id = :lineupId")
 	Optional<Lineup> findByIdWithUser(@Param("lineupId") Long id);
 
 	@Query("select l.restaurant.id from Lineup l where l.id = :lineupId")
 	Optional<Long> findRestaurantIdById(@Param("lineupId") Long id);
-
-	@Query("select l from Lineup l where l.user.id = :userId and l.restaurant.id = :restaurantId and l.status = :status")
-	Optional<Lineup> findByUserIdAndRestaurantIdAndStatus(@Param("userId") Long userId,
-		@Param("restaurantId") Long restaurantId, @Param("status") ArrivalStatusEnum statusEnum);
+	@Query("select l from Lineup l where l.user.id = :userId")
+	List<Lineup> findAllByUserId(@Param("userId") Long userId);
 
 	@Query("select l from Lineup l where l.restaurant.id = :restaurantId")
 	List<Lineup> findByRestaurantId(@Param("restaurantId") Long id);
