ea7417dac72551e6ea31dee90893bd720aecacbb
mitoconcrete <xogns98@gmail.com>
2023-03-08 05:31:52 +0900

    [fix] 테스트 미통과 원인 해결

--- src/main/java/team/waitingcatch/app/restaurant/controller/SellerManagementController.java
+++ src/main/java/team/waitingcatch/app/restaurant/controller/SellerManagementController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
